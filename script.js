85947585ff1555ec153c77fc84eca3933b5e4bc6
unknown <you@example.com>
2021-11-26 14:48:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,79 @@
+const arrowDownOne = document.getElementById("arrowDownOne");
+const arrowUpOne = document.getElementById("arrowUpOne");
+const answerOne = document.getElementById("answerOne");
+
+arrowDownOne.addEventListener("click", () => {
+  answerOne.style.display = "block";
+  arrowDownOne.style.display = "none";
+  arrowUpOne.style.display = "block";
+});
+
+arrowUpOne.addEventListener("click", () => {
+  answerOne.style.display = "none";
+  arrowDownOne.style.display = "block";
+  arrowUpOne.style.display = "none";
+});
+
+const arrowDownTwo = document.getElementById("arrowDownTwo");
+const arrowUpTwo = document.getElementById("arrowUpTwo");
+const answerTwo = document.getElementById("answerTwo");
+
+arrowDownTwo.addEventListener("click", () => {
+  answerTwo.style.display = "block";
+  arrowDownTwo.style.display = "none";
+  arrowUpTwo.style.display = "block";
+});
+
+arrowUpTwo.addEventListener("click", () => {
+  answerTwo.style.display = "none";
+  arrowDownTwo.style.display = "block";
+  arrowUpTwo.style.display = "none";
+});
+
+const arrowDownThree = document.getElementById("arrowDownThree");
+const arrowUpThree = document.getElementById("arrowUpThree");
+const answerThree = document.getElementById("answerThree");
+
+arrowDownThree.addEventListener("click", () => {
+  answerThree.style.display = "block";
+  arrowDownThree.style.display = "none";
+  arrowUpThree.style.display = "block";
+});
+
+arrowUpThree.addEventListener("click", () => {
+  answerThree.style.display = "none";
+  arrowDownThree.style.display = "block";
+  arrowUpThree.style.display = "none";
+});
+
+const arrowDownFour = document.getElementById("arrowDownFour");
+const arrowUpFour = document.getElementById("arrowUpFour");
+const answerFour = document.getElementById("answerFour");
+
+arrowDownFour.addEventListener("click", () => {
+  answerFour.style.display = "block";
+  arrowDownFour.style.display = "none";
+  arrowUpFour.style.display = "block";
+});
+
+arrowUpFour.addEventListener("click", () => {
+  answerFour.style.display = "none";
+  arrowDownFour.style.display = "block";
+  arrowUpFour.style.display = "none";
+});
+
+const arrowDownFive = document.getElementById("arrowDownFive");
+const arrowUpFive = document.getElementById("arrowUpFive");
+const answerFive = document.getElementById("answerFive");
+
+arrowDownFive.addEventListener("click", () => {
+  answerFive.style.display = "block";
+  arrowDownFive.style.display = "none";
+  arrowUpFive.style.display = "block";
+});
+
+arrowUpFive.addEventListener("click", () => {
+  answerFive.style.display = "none";
+  arrowDownFive.style.display = "block";
+  arrowUpFive.style.display = "none";
+});
